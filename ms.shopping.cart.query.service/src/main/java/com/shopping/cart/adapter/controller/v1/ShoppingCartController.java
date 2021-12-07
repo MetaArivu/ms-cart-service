15f8ae2f5a8223a44bd4a00e481dd4d52d2757d2
ketangote <ketan.gote@metamagic.in>
2021-12-07 10:59:35 +0530

    Changes: Cart Service Updated..

--- ms.shopping.cart.query.service/src/main/java/com/shopping/cart/adapter/controller/v1/ShoppingCartController.java
+++ ms.shopping.cart.query.service/src/main/java/com/shopping/cart/adapter/controller/v1/ShoppingCartController.java
@@ -4,6 +4,9 @@ import static com.shopping.cart.APPConstant.V1;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shopping.cart.adapter.entities.ShoppingCart;
+import com.shopping.cart.adapter.service.CartQueryServiceImpl;
 import com.shopping.cart.domainlayer.service.CartQueryService;
 import com.shopping.cart.dto.Response;
+import com.shopping.cart.server.secutiry.JWTUtil;
 
 import reactor.core.publisher.Mono;
 
@@ -24,13 +29,20 @@ import reactor.core.publisher.Mono;
 @CrossOrigin("*")
 public class ShoppingCartController {
 
+	private static final Logger log = (Logger) LoggerFactory.getLogger(ShoppingCartController.class);
+
 	@Autowired
 	private CartQueryService shoppingCartService;
 
-	@GetMapping(value = "/{id}")
-	public Mono<ResponseEntity<Response<List<ShoppingCart>>>> productDetailsById(@PathVariable("id") String id) {
-
-		return shoppingCartService.findByCustomerId(id).collectList()
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@GetMapping(value = "/")
+	public Mono<ResponseEntity<Response<List<ShoppingCart>>>> cartByUserId() {
+		String userId = jwtUtil.getUserIdFromToken(MDC.get("Authorization"));
+		log.info("Fetch Cart By  UserId={}",userId);
+		
+		return shoppingCartService.findByCustomerId(userId).collectList()
 				.map(list -> new ResponseEntity<Response<List<ShoppingCart>>>(
 						new Response<List<ShoppingCart>>(true, "Record retrieved successully", list), HttpStatus.OK))
 				.defaultIfEmpty(new ResponseEntity<Response<List<ShoppingCart>>>(
