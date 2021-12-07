50c199a8e7e99bd6e22df15f0830a2630296041b
ketangote <ketan.gote@metamagic.in>
2021-12-07 20:31:47 +0530

    Changes: Fine Tune Logs and Corrected Logic In Enhancer Service..

--- ms.shopping.cart.enhancer.service/src/main/java/com/shopping/cart/service/CartEnrichService.java
+++ ms.shopping.cart.enhancer.service/src/main/java/com/shopping/cart/service/CartEnrichService.java
@@ -12,6 +12,7 @@ import org.springframework.kafka.support.serializer.JsonSerde;
 import org.springframework.stereotype.Service;
 
 import com.shopping.cart.event.CartEvent;
+import com.shopping.cart.event.EventType;
 import com.shopping.cart.event.ProductEvent;
 
 import lombok.extern.log4j.Log4j2;
@@ -28,21 +29,26 @@ public class CartEnrichService {
 
 		cartEventStream.foreach((k, v) -> log.info("Key={}, Value={}", k, v));
 
-		cartEventStream.map((k, v) -> {
+		KStream<String, CartEvent> updatedCartEventStream = cartEventStream.map((k, v) -> {
 			CartEvent event = CartEvent.parse(v);
 			return KeyValue.pair(event.getItemId(), event);
-		})
-		.peek((k,v) -> log.info("Cart Event Key={}, Value={}",k,v))
-		.join(products,
-			  (productId, cartEvent)-> productId,
-			  (cartEvent, _product)-> {
-				  ProductEvent prodEvent = ProductEvent.parse(_product);
-				  return new CartEvent(cartEvent.getCustomerId(), cartEvent.getItemId(), 
-						  prodEvent.getName(), prodEvent.getPrice(), 
-						  cartEvent.getQty(), cartEvent.getEventType(), prodEvent.getImage());
-			  })
-		.peek((k,v) -> log.info("Enhanced Cart Key: {}, Value: {}", k, v))
-		.to("cart-details-event", Produced.with(Serdes.String(), new JsonSerde<>(CartEvent.class)));
-		
+		}).peek((k, v) -> log.info("Cart Event Key={}, Value={}", k, v));
+
+		updatedCartEventStream.filter((k, v) -> {
+			return v.getEventType().equals(EventType.CLEAR_CART);
+		}).map((k, v) -> {
+			CartEvent event = new CartEvent(v.getCustomerId(), v.getItemId(), "NA", 0.0, v.getQty(), v.getEventType(),"NA");
+			return KeyValue.pair(event.getItemId(), event);
+		}).to("cart-details-event", Produced.with(Serdes.String(), new JsonSerde<>(CartEvent.class)));
+
+		updatedCartEventStream.filter((k, v) -> {
+			return v.getEventType().equals(EventType.ADD_ITEM) || v.getEventType().equals(EventType.REMOVE_ITEM);
+		}).join(products, (productId, cartEvent) -> productId, (cartEvent, _product) -> {
+			ProductEvent prodEvent = ProductEvent.parse(_product);
+			return new CartEvent(cartEvent.getCustomerId(), cartEvent.getItemId(), prodEvent.getName(),
+					prodEvent.getPrice(), cartEvent.getQty(), cartEvent.getEventType(), prodEvent.getImage());
+		}).peek((k, v) -> log.info("Enhanced Cart Key: {}, Value: {}", k, v)).to("cart-details-event",
+				Produced.with(Serdes.String(), new JsonSerde<>(CartEvent.class)));
+
 	}
 }
